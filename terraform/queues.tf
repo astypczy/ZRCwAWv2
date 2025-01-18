da990551e47dca323c37d79aa99a1c83299890d7
Ubuntu <ubuntu@ip-10-0-8-188.ec2.internal>
2025-01-18 19:33:46 +0000

    Working

--- terraform/queues.tf
+++ terraform/queues.tf
@@ -1,95 +1,59 @@
-# 1. Tworzenie kolejki SQS
-resource "aws_sqs_queue" "messages_queue" {
-  name                        = "messages-queue"
-  visibility_timeout_seconds  = 10  
-  message_retention_seconds   = 86400  
-  max_message_size            = 262144  
-  receive_wait_time_seconds   = 0
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.dlq_queue.arn
-    maxReceiveCount     = 5
-  })
+# Kolejka SQS: message-queue
+resource "aws_sqs_queue" "message_queue" {
+  name                      = "message-queue"
+  delay_seconds             = 0
+  message_retention_seconds = 86400
 }
 
-resource "aws_sqs_queue" "dlq_queue" {
-  name = "messages-dlq"
-}
-
+# Kolejka SQS: admin-queue
 resource "aws_sqs_queue" "admin_queue" {
-  name = "admin-queue"
+  name                      = "admin-queue"
+  delay_seconds             = 0
+  message_retention_seconds = 86400
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
+# Funkcja Lambda
+resource "aws_lambda_function" "message_processor" {
+  filename         = "lambda_function.zip"                              # Plik ZIP z kodem
+  function_name    = "message_processor"
+  role             = "arn:aws:iam::637423541704:role/LabRole"    # ARN roli IAM
+  handler          = "lambda_function.lambda_handler"            # Handler
+  runtime          = "python3.10"                                # Wersja Pythona
+  timeout          = 10                                          # Timeout funkcji
+  memory_size      = 128                                         # Rozmiar pamięci
+  publish          = true
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Dodanie polityk do roli Lambda
-resource "aws_iam_policy_attachment" "lambda_execution_policy" {
-  name       = "lambda_execution_policy"
-  roles      = [aws_iam_role.lambda_execution_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy" "lambda_sqs_access" {
-  name = "lambda_sqs_access"
-
-  role = aws_iam_role.lambda_execution_role.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "sqs:ReceiveMessage",
-          "sqs:DeleteMessage",
-          "sqs:GetQueueAttributes",
-          "sqs:SendMessage"
-        ],
-        Resource = [
-          aws_sqs_queue.messages_queue.arn,
-          aws_sqs_queue.dlq_queue.arn
-        ]
-      }
-    ]
-  })
-}
-
-# 3. Funkcja Lambda
-resource "aws_lambda_function" "process_messages" {
-  filename         = "lambda_function.zip" # Plik ZIP zawierający kod Lambdy
-  function_name    = "process_messages_lambda"
-  role             = aws_iam_role.lambda_execution_role.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
-  timeout          = 15  # Maksymalny czas wykonania Lambdy (w sekundach)
-  memory_size      = 128
-  reserved_concurrent_executions = 2  # Maksymalna liczba równoczesnych instancji
-
+  # Zmienne środowiskowe
   environment {
     variables = {
-      SOURCE_QUEUE_URL = aws_sqs_queue.messages_queue.id
-      ADMIN_QUEUE_URL  = aws_sqs_queue.admin_queue.id
+      QUEUE_URL       = aws_sqs_queue.message_queue.id           # URL message-queue
+      ADMIN_QUEUE_URL = aws_sqs_queue.admin_queue.id             # URL admin-queue
     }
   }
 }
 
-# 4. Wyzwalacz SQS dla Lambdy
+# Powiązanie Lambdy z kolejką message-queue (trigger)
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
-  event_source_arn  = aws_sqs_queue.messages_queue.arn
-  function_name     = aws_lambda_function.process_messages.arn
-  batch_size        = 1  # Maksymalnie jedna wiadomość na raz
-  enabled           = true
+  event_source_arn = aws_sqs_queue.message_queue.arn             # Źródło: message-queue
+  function_name    = aws_lambda_function.message_processor.arn   # Funkcja Lambda
+  batch_size       = 1                                           # Jedna wiadomość na raz
+  enabled          = true
+}
+
+data "aws_lambda_function" "lambda_latest_version" {
+  function_name = aws_lambda_function.message_processor.function_name
+}
+
+# Konfiguracja wywoływania zdarzeń Lambdy
+resource "aws_lambda_function_event_invoke_config" "lambda_config" {
+  function_name                  = aws_lambda_function.message_processor.function_name
+  maximum_retry_attempts         = 2                            # Liczba prób w razie błędu
+  maximum_event_age_in_seconds   = 60                           # Maksymalny czas na przetworzenie zdarzenia
+}
+
+# Ograniczenie równoczesności Lambdy (do 2 instancji)
+resource "aws_lambda_provisioned_concurrency_config" "concurrency" {
+  function_name                     = aws_lambda_function.message_processor.function_name
+  qualifier                         = data.aws_lambda_function.lambda_latest_version.version # Użycie najnowszej wersji
+  provisioned_concurrent_executions = 2
 }
